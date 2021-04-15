71041a93e61f11137a57ff3d9b19c926128ba585
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-15 17:31:53 +0200

    finito con l'aiuto di lorenzo

--- js/script.js
+++ js/script.js
@@ -39,6 +39,14 @@ $('document').ready(function(){
         console.log(nome, peso)
     }
     
+
+    // Snack 2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
     //GENERAZIONE NUMERI RANDOM
     const randomNumber = (min, max) => {
         min = Math.ceil(min);
